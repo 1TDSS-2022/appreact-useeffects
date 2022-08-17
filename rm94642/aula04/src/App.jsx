48f795371589b0cebbd5fb3f4cd737cfb73a5d75
matlencar <rm94642@fiap.com.br>
2022-08-17 20:56:02 -0300

    primeiro exemplo com useEffect.

---
+++ rm94642/aula04/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Api from './components/api/Api'
+
+
+export default function App() {
+    return(
+        <div>
+            <h1>Estudo sobre use-effects</h1>
+            <Api/>
+        </div>
+    )
+} 
