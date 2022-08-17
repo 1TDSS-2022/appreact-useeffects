7f43fd224287913a5e4bb9c59fee4071d55f7484
giovana-abreu <rm95896@fiap.com.br>
2022-08-17 20:34:35 -0300

    Até criação da API

---
+++ RM95896/AULA04/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function App(){
+    return(
+        <div>
+            <h1>Estudo sobre USE EFFECT</h1>
+        </div>
+    )
+
+// const App = () => (
+    
+// //     return()
+// return(
+//     <div>
+//         <h1>Estudo sobre USE EFFECT</h1>
+//     </div>
+// //    )
+}
---
+++ RM95896/AULA04/src/components/api/api.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Api (){
+    return(
+        <div>
+            <h2>Lista de usuarios do Git Hub</h2>
+
+        </div>
+    )
+}
