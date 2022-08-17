79aa19264b4e88336734a46980abda01bcb133e7
LucasKenji01 <rm94233@fiap.com.br>
2022-08-17 20:56:04 -0300

    1º Exemplo de Use Effect

---
+++ rm94233/aula04/src/components/api/Api.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { useEffect, useState } from "react"
+
+export default function Api(){
+
+    const [repositorios, setRepositorios] = useState([])
+
+    useEffect(()=>{
+        console.log("RENDERIZOU!")
+    },[])
+
+    useEffect(()=>{
+        console.log("RENDERIZOU O OBJETO")
+    },[repositorios])
+
+
+    return(
+        <div>
+            <h2>Lista de Usuário do GitHUB</h2>
+        <button onClick={()=> setRepositorios(["Alê", "Ronqui"])}>ALTERAR</button>
+        </div>
+    )
+}
