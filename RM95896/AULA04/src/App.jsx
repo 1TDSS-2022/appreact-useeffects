7dd6fa34e11b13e9d653aa3181f837c4881a55be
giovana-abreu <rm95896@fiap.com.br>
2022-08-17 20:56:05 -0300

    Primeiro exemplo de UseEffect

--- RM95896/AULA04/src/App.jsx
+++ RM95896/AULA04/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import Api from "./components/API/API"
 
 export default function App(){
     return(
         <div>
             <h1>Estudo sobre USE EFFECT</h1>
+            <Api/>
         </div>
     )
 
--- RM95896/AULA04/src/components/api/api.jsx
+++ RM95896/AULA04/src/components/api/api.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 export default function Api (){
+
+    const [repositorios,setRepositorios] = useState([])
+
+    useEffect(()=>{
+        console.log("Renderizou!")
+    },[])
+
+    useEffect(()=>{
+        console.log("Renderizou O OBJETO!")
+    },[repositorios])
+
     return(
         <div>
             <h2>Lista de usuarios do Git Hub</h2>
+            <button onClick={()=> setRepositorios(['Ale', 'Ronqui'])}>ALTERAR</button>
 
         </div>
     )
