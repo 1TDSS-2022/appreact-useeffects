95a8692415fb4606fa2da9c8361983999d601811
EduardaVigo <rm93596@fiap.com.br>
2022-08-17 21:07:43 -0300

    Exemplo Final UserEffects

--- rm93596/aula04/src/components/api/Api.jsx
+++ rm93596/aula04/src/components/api/Api.jsx
@@ -5,17 +5,39 @@ export default function App(){
 
     const [repositorios, setRepositorios] = useState([])
 
-    useEffect(()=>{
-        console.log("RENDERIZOU!")
-    })
-    useEffect(()=>{
-        console.log("RENDERIZOU O OBJETO!")
-    },[repositorios])
+    useEffect(() => {      
+        carregaRepos()
+    },[])
+
+
+    const carregaRepos = async() =>{
+        const response = await fetch("https://api.github.com/users")
+        const data = await response.json()
+
+        setRepositorios(data)
+    }
+
+
+
+
+
+    // useEffect(()=>{
+    //     console.log("RENDERIZOU!")
+    // })
+    // useEffect(()=>{
+    //     console.log("RENDERIZOU O OBJETO!")
+    // },[repositorios])
 
     return(
         <div>
             <h2>Lista de Usuários do GitHUB</h2>
-            <button onClick={()=> setRepositorios(["Alê, Ronqui"])}>ALTERAR</button>
+            {/* <button onClick={()=> setRepositorios(["Alê, Ronqui"])}>ALTERAR</button> */}
+            <ul>
+                {
+                    repositorios.map((obj, i)=>
+                    <li key={i}>{obj.login}</li>
+                    )}
+            </ul>
         </div>
     )
 }
