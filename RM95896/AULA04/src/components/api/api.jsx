ca4935e83e37d0250f80d63a719aee9b7f6f574e
giovana-abreu <rm95896@fiap.com.br>
2022-08-17 21:11:22 -0300

    COMMIT FINAL

--- RM95896/AULA04/src/components/api/api.jsx
+++ RM95896/AULA04/src/components/api/api.jsx
@@ -2,23 +2,44 @@ import React from 'react'
 import { useState } from 'react'
 import { useEffect } from 'react'
 
+
 export default function Api (){
-
     const [repositorios,setRepositorios] = useState([])
+    useEffect= (() => {
+    carregaRepos()
+}, [])
 
-    useEffect(()=>{
-        console.log("Renderizou!")
-    },[])
+    const carregaRepos = async() =>{
+            const response = await fetch('https://api.github.com/users')
+            const data = await response.json()
+    
+            setRepositorios(data)
 
-    useEffect(()=>{
-        console.log("Renderizou O OBJETO!")
-    },[repositorios])
+    }
+
+    // useEffect(()=>{
+    //     console.log("Renderizou!")
+    // },[])
+
+    // useEffect(()=>{
+    //     console.log("Renderizou O OBJETO!")
+    // },[repositorios])
 
     return(
         <div>
             <h2>Lista de usuarios do Git Hub</h2>
-            <button onClick={()=> setRepositorios(['Ale', 'Ronqui'])}>ALTERAR</button>
-
+            {/* <button onClick={()=> setRepositorios(['Ale', 'Ronqui'])}>ALTERAR</button> */}
+        <ul>
+            {repositorios.map((obj,i)=>
+            <li key = {i}>{obj.login}</li>
+            )}
+        </ul>
         </div>
     )
 }
+
+// EXERCICIO:
+
+// 1. RECUPERAR O AVATAR DO USUARIO
+// 2. TROCAR A KEY DO MAP PELO ID DO USER
+// 3. MUDAR A ESTRUTURA DENTRO DA LI PRA UMA ESTRUTURA SEMANTICA COM FIG/IMG/FIGCAPTION
