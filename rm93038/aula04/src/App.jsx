0cc231d05d16d1d69adbda0af885102baa31d499
Breno <rm93038@fiap.com.br>
2022-08-17 23:28:03 -0300

    .

---
+++ rm93038/aula04/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+// export default function App(){
+
+//     return(
+//         <div>
+//             <h1>Estudo sobre USE-EFFECTS</h1>
+//         </div>
+//     )
+
+// }
+
+const App () => {
+    return(
+        <div>
+            <h1>Estudo sobre USE-EFFECTS</h1>
+        </div>
+    )
+    
+}
