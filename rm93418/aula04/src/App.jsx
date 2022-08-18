9d6a880455c8473585a2aec51d4d4f894a3223cd
Rodrigo Santos <rm93418@fiap.com.br>
2022-08-17 21:09:38 -0300

    fim

---
+++ rm93418/aula04/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Api from "./compnents/api/Api";
+
+export default function App(){
+    return(
+        <div>
+            <h1>ESTUDO SOBRE USE-EFFECTS</h1>
+            <Api>
+        </div>
+    )
+}
