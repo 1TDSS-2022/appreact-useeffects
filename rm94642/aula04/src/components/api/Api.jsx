12c632f26f26f7f8d309915f8e6dbbc7617ce081
matlencar <rm94642@fiap.com.br>
2022-08-17 21:07:49 -0300

    Exercicio

--- rm94642/aula04/src/components/api/Api.jsx
+++ rm94642/aula04/src/components/api/Api.jsx
@@ -6,18 +6,32 @@ export default function Api(){
 
     const [repositorios, setRepositorios] = useState([])
 
-    useEffect(()=>{
-        console.log("Renderizou")
-    })
 
     useEffect(()=>{
-        console.log("Renderizou o objeto")
-    },[repositorios])
+        carregaRepos()
+    },[])
+
+    const carregaRepos = async () =>{
+        const response = await fetch("https://api.github.com/users")
+        const data = await response.json()
+
+        setRepositorios(data)
+    }
 
     return(
         <div>
             <h2>Lista de Usuarios do GitHUB</h2>
-            <button onClick={()=>setRepositorios(["Ale", "Ronqui"])}>Alterar</button>
+            {/*<button onClick={()=>setRepositorios(["Ale", "Ronqui"])}>Alterar</button>*/}
+            <ul>
+                {repositorios.map((obj, i)=>
+                <li key={i}>{obj.login}</li>
+                )}
+            </ul>
         </div>
     )
 }
+
+Exercicio:
+1 - Recuperar o avatar do usuario 
+2 - Trocar a key do map pelo id do user 
+3 - Mudar a estrutura dentro da li para uma estrutura semantica com figure/img/
