5eed72ddcbe9865288c79d8810e83f61360afc05
JaelsonJonas <rm93915@fiap.com.br>
2022-08-17 21:09:54 -0300

    UPDATE project 2

--- rm93915/Aula04/src/components/api/Api.jsx
+++ rm93915/Aula04/src/components/api/Api.jsx
@@ -5,19 +5,39 @@ import { useEffect } from "react";
 export default function Api(){
 
     const [repositorios, setRepositorios] = useState([])
-    useEffect(()=>{
-        console.log("Renderizou")
-
+    useEffect(() => {
+        carregaRepos()
     })
+    
+    const carregaRepos = async() =>{
+        const response = await fetch("https://api.github.com/users")
+        const data  = await response.json();
 
-    useEffect(()=>{
-        console.log("Renderizou o obj")
+       setRepositorios(data)
+    }
+    // useEffect(()=>{
+    //     console.log("Renderizou")
 
-    },[repositorios])
+    // })
+
+    // useEffect(()=>{
+    //     console.log("Renderizou o obj")
+
+    // },[repositorios])
+
+    // exericios:
+    // 1) recuperar o avatar do usuario
+    // 2) trocar a Key do map pelo o id do user
+    // 3) mudar a estrutura dentro da li para uma estrutura  semantica com figure/img/figcapture
     return(
         <div>
             <h2>Lista de usuario do GITHUB</h2>
-            <button onClick= {() => setRepositorios(["jow","olá"])}>GAbiru</button>
+            {/* <button onClick= {() => setRepositorios(["jow","olá"])}>GAbiru</button> */}
+            <ul>
+            {repositorios.map((obj,i)=>
+            
+            <li key = {i}> {obj.login} </li>)}
+            </ul>
         </div>
     )
 }
