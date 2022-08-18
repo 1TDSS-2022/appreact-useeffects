4a7f2d6273cc417ec8a6771697da46f4d112efb9
FilipeJava <RM93042@fiap.com.br>
2022-08-17 21:07:39 -0300

    realizar exercicio

--- rm93042/aula04/src/components/api/Api.jsx
+++ rm93042/aula04/src/components/api/Api.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 
@@ -7,22 +7,40 @@ import { useEffect } from "react";
 export default function Api(){
 
     const [repositorios,setRepositorios]=useState([])
+
+    useEffect(()=>{
+        carregaRepos()
+    },[])
+       
+    const carregaRepos = async()=>{
+        const reponse = await fetch("https://api.github.com/users")
+        const data = await reponse.json()
+
+        setRepositorios(data)
+    }
+
     
     //o Use Effect executa uma unica vez no Carregamento do componente
-    useEffect(()=>{
-        console.log("Render")
+    // useEffect(()=>{
+    //     console.log("Render")
 
-    },[])
+    // },[])
 
-    useEffect(()=>{
-        console.log("Render O Objeto")
+    // useEffect(()=>{
+    //     console.log("Render O Objeto")
 
-    },[repositorios])
+    // },[repositorios])
 
     return(
         <div>
             <h2>Lista de Usuarios do GitHUB</h2>
-            <button onClick={()=> setRepositorios(["Filipe","Fran"])}>Alterar</button>
+            {/* <button onClick={()=> setRepositorios(["Filipe","Fran"])}>Alterar</button> */}
+
+            <ul>
+                {repositorios.map((obj,i)=>
+                <li key={i}>{obj.login}</li>
+                )}
+            </ul>
         </div>
     )
 }
