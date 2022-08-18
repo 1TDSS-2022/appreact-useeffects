cbe422fd0c8be2398eac4db0cbeca0605cc0c296
Nakamura16 <andre.massao.nakamura@gmail.com>
2022-08-17 21:09:10 -0300

    acessando a API

--- rm93046/aula04/src/components/api/Api.jsx
+++ rm93046/aula04/src/components/api/Api.jsx
@@ -15,10 +15,27 @@ export default function Api(){
         console.log("Renderizou o objeto")
     },[repositorios])
 
+    useEffect( ()=>{
+        carregaRepos()
+    },[])
+
+    const carregaRepos = async ()=>{
+        const response = await fetch("https://api.github.com/users")
+        const data = await response.json()
+
+        setRepositorios(data)
+    }
+
     return(
         <div>
             <h2>Lista de usuários do github</h2>
             <button onClick={()=> setRepositorios(["Alê","Ronqui"])}>Alterar</button>
+
+            <ul>
+                {repositorios.map((obj,i)=>
+                <li key={i}>{obj.login}</li>
+                )}
+            </ul>
         </div>
     )
 }
