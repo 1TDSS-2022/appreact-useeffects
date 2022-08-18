dad9a01eec93c4b98a0871a999a95525c6b03d91
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-17 21:07:30 -0300

    Exemplo final useEffects

--- pf0670/aula04/src/components/api/Api.jsx
+++ pf0670/aula04/src/components/api/Api.jsx
@@ -5,18 +5,41 @@ export default function Api(){
 
     const [repositorios, setRepositorios] = useState([])
 
-    useEffect(()=>{
-        console.log("RENDERIZOU!")
-    },[])
+    useEffect(() => {
+        carregaRepos()
+    }, [])
+    
+    const carregaRepos = async() =>{
+        const response = await fetch("https://api.github.com/users")
+        const data = await response.json()
+        setRepositorios(data)
 
-    useEffect(()=>{
-        console.log("RENDERIZOU O OBJETO")
-    },[repositorios])
+    }
+    
+    // useEffect(()=>{
+    //     console.log("RENDERIZOU!")
+    // },[])
+
+    // useEffect(()=>{
+    //     console.log("RENDERIZOU O OBJETO")
+    // },[repositorios])
+
+    // Exercício:
+    // 1 - Recuperar o avatar do usuário
+    // 2 - Trocara a Key do map pelo id do user
+    // 3 - Mudar a estrutura dentro da li para uma estrutura semântica com figure/img/figcaption
+
+
 
     return(
         <div>
             <h2>Lista de Usuário do GitHUB</h2>
-            <button onClick={()=> setRepositorios(["Alê","Ronqui"])}>ALTERAR</button>
+            {/* <button onClick={()=> setRepositorios(["Alê","Ronqui"])}>ALTERAR</button> */}
+            <ul>
+                {repositorios.map((obj,i)=>
+                    <li key={i}>{obj.login}</li>
+                )}
+            </ul>
         </div>
     )
 }
