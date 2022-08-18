0d35e7d208df7e1c2a46eaf541746e28dba4f151
krinaf <rm93024@fiap.com.br>
2022-08-17 21:08:15 -0300

    Exemplo final useEffects

--- rm93024/aula04/src/components/api/Api.jsx
+++ rm93024/aula04/src/components/api/Api.jsx
@@ -6,17 +6,32 @@ export default function Api() {
     const [repositorios, setRepositorios] = useState([])
 
     useEffect(() => {
-        console.log("Renderizou o componente!")                     
+        carregaRepos()
     },[])
 
-    useEffect(() => {
-        console.log("Renderizou o objeto!")
-    },[repositorios])
+    const carregaRepos = async() => {
+        const response  = await fetch("https://api.github.com/users")
+        const data = await response.json()
+        setRepositorios(data)    
+    }
+
+    // useEffect(() => {
+    //     console.log("Renderizou o componente!")                     
+    // },[])
+
+    // useEffect(() => {
+    //     console.log("Renderizou o objeto!")
+    // },[repositorios])
     
     return(
         <div>
             <h2>Lista de Usuários do GitHub</h2>
-            <button onClick={()=> setRepositorios(["Alê","Salvio"])}>ALTERAR</button>
+            {/* <button onClick={()=> setRepositorios(["Alê","Salvio"])}>ALTERAR</button> */}
+            <ul>
+                {repositorios.map((obj,i)=>
+                    <li key={i}>{obj.login}</li>
+                )}
+            </ul>
         </div>
     )
 }
