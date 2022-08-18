09645234387ebc63469ac7ad669e0cbf0260f363
rm93586 <rm93586@fiap.com.br>
2022-08-17 21:07:44 -0300

    Exemplo final userEffects

--- rm93586/aula04/src/components/api/Api.jsx
+++ rm93586/aula04/src/components/api/Api.jsx
@@ -5,26 +5,48 @@ export default function Api(){
 
     const [repositorios, setRepositorios] = useState([])
 
+    useEffect( () => {
+        carregaRepos()
+    }, [])
+
+    const carregaRepos = async() => {
+
+        const response = await fetch("https://api.github.com/users")
+        const data = await response.json()
+
+        setRepositorios(data)
+
+    }
+
     //Quando o componente se montar ele usará o que está dentro da area abaixo
     //O useEffect executa uma unica vez. No carregament odo comp.
     //Criado apenas uma vez
-    useEffect(()=> {
-        console.log("Renderizou!")
+    // useEffect(()=> {
+    //     console.log("Renderizou!")
         
-    },[])
+    // },[])
 
 
-    //Será recarregado toda vez que atualizar ao apertar o botão
-    useEffect(()=> {
-        console.log("Renderizou o objeto!")
+    // //Será recarregado toda vez que atualizar ao apertar o botão
+    // useEffect(()=> {
+    //     console.log("Renderizou o objeto!")
         
-    },[repositorios])
+    // },[repositorios])
 
+    //Exercicio
+    //1 - Recuperar o avatar do usuário
+    //2 - Trocar a Key do map pelo id do user
+    //3 - Mudar a estrutura dentro da li para uma estrutura semântica com figure/img/figure
 
     return (
         <div>
             <h2>Lista de Usuário do GITHUB</h2>
-            <button onClick={()=> setRepositorios(["Alê","Ronqui"])}>ALTERAR</button>
+            {/* <button onClick={()=> setRepositorios(["Alê","Ronqui"])}>ALTERAR</button> */}
+            <ul>
+                {repositorios.map((obj, i) => 
+                    <li key={i}>{obj.login}</li>
+                )}
+            </ul>
         </div>
     )
 }
