023770c70a806b5486bed036643601c2027afba6
PedroSanagiotto <rm93182@fiap.com.br>
2022-08-17 21:15:33 -0300

    exemplos useEffect

--- rm93182/aula04/src/index.js
+++ rm93182/aula04/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
 
-ReactDom.render("CONTEÚDO", document.getElementById("root"))
+ReactDOM.render(<App/>, document.getElementById("root"))
