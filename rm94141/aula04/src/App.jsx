d59858ab04ce5db1815307f1ac851a52ff86deaa
mariananieton <rm94141@fiap.com.br>
2022-08-24 20:46:05 -0300

    Criando rotas

--- rm94141/aula04/src/App.jsx
+++ rm94141/aula04/src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import Api from './components/api/Api'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/home/Home'
+import Produtos from './components/produtos/Produtos'
+import Servicos from './components/servicos/Servicos'
 
 export default function App (){
     return (
-        <div>
-            <h1>ESTUDO SOBRE USE-EFFECTS</h1>
-            <Api/>
-        </div>
+       
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="/produtos" element={<Produtos/>}></Route>
+                <Route path="/servicos" element={<Servicos/>}></Route>
+            </Routes>
+        </BrowserRouter>
+
     )
 }
