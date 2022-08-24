dd484ab1444521cc77a5a7bd1a89c98233e1cafe
mouragus <gumora18@hotmail.com>
2022-08-24 20:39:03 -0300

    api update

--- rm94280/aula04/src/components/api/Api.jsx
+++ rm94280/aula04/src/components/api/Api.jsx
@@ -3,36 +3,71 @@ import { useEffect, useState } from "react"
 
 export default function Api(){
 
-    const[repositorios, setRepositorios] = useState([])
+    const objeto = [
+    
+    {
+        nome : "Nanne",
+        id : 121
+    }
+    ,
+    {
+        nome : "Julia",
+        id : 122
+    }
+    ,
+    {
+        nome : "Roberto",
+        id : 123
+    }
 
-    useEffect(()=> {
+]
+   
+
+
+
+    const [repositorios, setRepositorios] = useState([])
+
+    useEffect(() => {
         carregaRepos()
     }, [])
-
-    const carregaRepos = async() => {
+    
+    const carregaRepos = async() =>{
         const response = await fetch("https://api.github.com/users")
         const data = await response.json()
         setRepositorios(data)
-    }
 
-    // useEffect(()=>{
-    //     console.log("Renderizou!")
-    // },[])
+    }
     
     // useEffect(()=>{
-    //     console.log("Renderizou o objeto !")
+    //     console.log("RENDERIZOU!")
+    // },[])
+
+    // useEffect(()=>{
+    //     console.log("RENDERIZOU O OBJETO")
     // },[repositorios])
+
+    // Exercício:
+    // 1 - Recuperar o avatar do usuário
+    // 2 - Trocara a Key do map pelo id do user
+    // 3 - Mudar a estrutura dentro da li para uma 
+    //estrutura semântica com figure/img/figcaption
+
 
 
     return(
         <div>
-            <h2>Lista de Usuarios do GitHUB</h2>
-            {/* <button onClick={()=> setRepositorios(["Gusta", "Gilson"])}>ALTERAR</button> */}
-            <ul>
-                {repositorios.map((obj,i) => 
-                    <li key={i}>{obj.login}</li>)}
-            </ul>     
-        
+            <h2>Lista de Usuário do GitHUB</h2>
+            {/* <button onClick={()=> setRepositorios(["Alê","Ronqui"])}>ALTERAR</button> */}
+            <ul style={{listStyle:'none'}}>
+                {repositorios.map((obj)=>
+                    <li key={obj.id}>
+                        <figure>
+                            <img src={obj.avatar_url} alt="AVATAR" style={{width:'200px'}}/>
+                            <figcaption><a href={obj.html_url}>{obj.login}</a> </figcaption>
+                        </figure>
+                    </li>
+                )}
+            </ul>
         </div>
     )
 }
