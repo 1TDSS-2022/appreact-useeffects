ce1fbe6826c8d257ef0b2298fce9bc91b8baf18d
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-24 20:23:43 -0300

    |correção exercício API

--- pf0670/aula04/src/components/api/Api.jsx
+++ pf0670/aula04/src/components/api/Api.jsx
@@ -3,6 +3,28 @@ import { useEffect, useState } from "react"
 
 export default function Api(){
 
+    const objeto = [
+    
+    {
+        nome : "Nanne",
+        id : 121
+    }
+    ,
+    {
+        nome : "Julia",
+        id : 122
+    }
+    ,
+    {
+        nome : "Roberto",
+        id : 123
+    }
+
+]
+   
+
+
+
     const [repositorios, setRepositorios] = useState([])
 
     useEffect(() => {
@@ -27,7 +49,8 @@ export default function Api(){
     // Exercício:
     // 1 - Recuperar o avatar do usuário
     // 2 - Trocara a Key do map pelo id do user
-    // 3 - Mudar a estrutura dentro da li para uma estrutura semântica com figure/img/figcaption
+    // 3 - Mudar a estrutura dentro da li para uma 
+    //estrutura semântica com figure/img/figcaption
 
 
 
@@ -35,9 +58,14 @@ export default function Api(){
         <div>
             <h2>Lista de Usuário do GitHUB</h2>
             {/* <button onClick={()=> setRepositorios(["Alê","Ronqui"])}>ALTERAR</button> */}
-            <ul>
-                {repositorios.map((obj,i)=>
-                    <li key={i}>{obj.login}</li>
+            <ul style={{listStyle:'none'}}>
+                {repositorios.map((obj)=>
+                    <li key={obj.id}>
+                        <figure>
+                            <img src={obj.avatar_url} alt="AVATAR" style={{width:'200px'}}/>
+                            <figcaption><a href={obj.html_url}>{obj.login}</a> </figcaption>
+                        </figure>
+                    </li>
                 )}
             </ul>
         </div>
