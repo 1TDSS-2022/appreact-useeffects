1519798363639450957eba6ee83257e5904b76b0
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2022-08-24 20:45:43 -0300

    ROtas 1

--- pf0670/aula04/src/App.jsx
+++ pf0670/aula04/src/App.jsx
@@ -1,23 +1,19 @@
 import React from "react"
-import Api from "./components/api/Api"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/home/Home'
+import Produtos from "./components/produtos/Produtos"
+import Servicos from "./components/servicos/Servicos"
 
 export default function App(){
     return(
-        <div>
-            <h1>ESTUDO SOBRE USE-EFFECTS</h1>
-            <Api/>
-        </div>
-    )
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/produtos" element={<Produtos/>} />
+                <Route path="/servicos" element={<Servicos/>} />
+            </Routes>
+        </BrowserRouter>
+
+        )
 }
-
-
-//  const App = ()=>{
-  
-//     return(
-//         <div>
-//             <h1>ESTUDO SOBRE USE-EFFECTS</h1>
-//         </div>
-//     )
-//  }
-
-// export default App
