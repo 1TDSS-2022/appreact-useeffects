8f7a8588392247e859cdb825ddef9d21f38776b4
RodrigoMachadoC <rm94301@fiap.com.br>
2022-08-24 21:09:49 -0300

    criação

---
+++ rm94301/aula04/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Api from "./components/api/Api";
+
+export default function App(){
+    
+    return(
+        <div>
+            <h2>Lista de Usuários</h2>
+            <Api/>
+        </div>
+    )
+}
