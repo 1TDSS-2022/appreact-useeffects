a420b07bdcfb5cce6f6dadcbf2e432ae70ac7e53
rm93586 <rm93586@fiap.com.br>
2022-08-26 09:20:51 -0300

    adição do sobre

--- rm93586/loja-moda/src/components/sobre/Sobre.jsx
+++ rm93586/loja-moda/src/components/sobre/Sobre.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
+import './Sobre.css'
+
 export default function Sobre(){
     return(
         <>
-        <h1>Sobre a Página</h1>
+        <section class="sobre">
+            <article class="sobre-conteudo">
+            <h1>Alunos:</h1>
+            <p>RM93586 - William Barrence Santos Junior</p>
+            <br/>
+            <p>Eu "componentizei" a lista de roupas para poder reutilizar nas páginas: "moda feminina", "moda masculina" e "moda infantil".</p>
+            <p>utilizei o "props" para passar as informações</p>
+            <br/>
+            <p>O conteúdo utilizado foram capas de revistas e artigos de moda em geral na internet</p>
+            </article>
+        </section>
         </>
     )
 }
