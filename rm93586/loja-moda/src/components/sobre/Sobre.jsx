927724fc57d3cb9eddfaa3b4505e0f0af3a5c2e0
rm93586 <rm93586@fiap.com.br>
2022-08-26 21:02:00 -0300

    atualização do resumo no sobre

--- rm93586/loja-moda/src/components/sobre/Sobre.jsx
+++ rm93586/loja-moda/src/components/sobre/Sobre.jsx
@@ -11,7 +11,8 @@ export default function Sobre(){
             <p>RM93586 - William Barrence Santos Junior</p>
             <br/>
             <p>Eu "componentizei" a lista de roupas para poder reutilizar nas páginas: "moda feminina", "moda masculina" e "moda infantil".</p>
-            <p>utilizei o "props" para passar as informações</p>
+            <p>utilizei o "props" para passar as informações para o componente</p>
+            <p>utilizei useState no minha conta</p>
             <br/>
             <p>O conteúdo utilizado foram capas de revistas e artigos de moda em geral na internet</p>
             </article>
