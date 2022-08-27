d1464a7c8ff1edf333570b6b26be2c4bb9c48b64
Breno <rm93038@fiap.com.br>
2022-08-26 21:48:32 -0300

    exercicio loja modas

---
+++ aula04/exerciciolojamodas/src/components/produtos/Produtos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Produtos(props) {
+    return (
+        <div>
+            <figure>
+                <img src="" alt="" />
+            </figure>
+        </div>
+    )
+}
